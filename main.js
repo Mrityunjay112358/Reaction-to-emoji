e2a7e2938eff9b24eb8cd734086ea31c8eaeef42
Mrityunjay112358 <73526816+Mrityunjay112358@users.noreply.github.com>
2021-03-25 10:20:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,3 +18,60 @@ Webcam.snap(function(data_uri){
 });
 
 }
+
+
+console.log("ml5 version"+ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/-3WpjoD2w/model.json',modelLoaded)
+
+
+function modelLoaded(){
+    console.log("model loaded");
+}
+
+function speak(){
+     synth = window.speechSynthesis;
+speak_data="The First Prediction Is "+prediction1;
+speak_data2="And The Second Prediction Is"+prediction2;
+utterThis = new SpeechSynthesisUtterance(speak_data+speak_data2);
+synth.speak(utterThis);
+}
+
+function predict_emotion(){
+img = document.getElementById("captured_img");
+classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+if(error){
+    console.error(error);
+}
+else{
+    console.log(results);
+    prediction1=results[0].label;
+prediction2 = results[1].label;
+document.getElementById("emotion_prediction_1").innerHTML=prediction1;
+document.getElementById("emotion_prediction_2").innerHTML=prediction2;
+
+speak();
+
+if(prediction1 == "Cool"){
+    document.getElementById("emoji_prediction_1").innerHTML="&#129311;";
+}
+if(prediction1 == "Awesome"){
+    document.getElementById("emoji_prediction_1").innerHTML="&#128076;";
+}
+if(prediction1 == "Thumbs Up"){
+    document.getElementById("emoji_prediction_1").innerHTML="&#128077;";
+}
+if(prediction2 == "Cool"){
+    document.getElementById("emoji_prediction_2").innerHTML="&#129311;";
+}
+if(prediction2 == "Awesome"){
+    document.getElementById("emoji_prediction_2").innerHTML="&#128076;";
+}
+if(prediction2 == "Thumbs Up"){
+    document.getElementById("emoji_prediction_2").innerHTML="&#128077;";
+}
+}
+}
